7205c02dab2151bf409750c30a2ff320ad8d5325
liuruichao <liuruichao555@126.com>
2017-03-14 17:37:25 +0800

    访问java bean私有属性

--- src/main/java/reflect/ReflectTest1.java
+++ src/main/java/reflect/ReflectTest1.java
@@ -1,5 +1,7 @@
 package reflect;
 
+import java.lang.reflect.Field;
+
 /**
  * ReflectTest1
  *
@@ -7,25 +9,28 @@ package reflect;
  * Created on 2017/3/14 17:28
  */
 public class ReflectTest1 {
-    public static void main(String[] args) {
-        System.out.println(Person.getInstance());
+    public static void main(String[] args) throws IllegalAccessException {
+        Person person = new Person(1, "liuruichao", 20);
+        Class<?> clazz = Person.class;
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            // 抑制Java对修饰符的检查
+            field.setAccessible(true);
+            System.out.println(field.get(person));
+        }
     }
 
     private static class Person {
+        private int id;
+
         private String name;
 
         private int age;
 
-        private Person () {
-            System.out.println("person");
-        }
-
-        private static class PersonHolder {
-            private static final Person person = new Person();
-        }
-
-        public static Person getInstance() {
-            return PersonHolder.person;
+        Person(int id, String name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
         }
     }
 }
