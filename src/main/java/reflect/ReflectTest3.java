cbd14d1dd018d38d1f0f738d9ff34e69bf7974be
liuruichao <liuruichao555@126.com>
2017-04-02 22:19:18 +0800

    add method invoke

---
+++ src/main/java/reflect/ReflectTest3.java
@@ -0,0 +1,38 @@
+package reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * ReflectTest3
+ *
+ * @author liuruichao
+ * Created on 2017/4/2 22:13
+ */
+public class ReflectTest3 {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Dog dog = new Dog("xiaohei", 2);
+        Class<Dog> clazz = Dog.class;
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.getName().equals("print")) {
+                method.invoke(dog);
+            }
+        }
+    }
+
+    private static class Dog {
+        private String name;
+
+        private int age;
+
+        public Dog(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void print() {
+            System.out.println(String.format("name: %s, age: %s.", name, age));
+        }
+    }
+}
