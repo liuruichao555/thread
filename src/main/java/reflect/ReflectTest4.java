34af7e5ec106b8eba025f9620ced74627d85e8aa
liuruichao <liuruichao555@126.com>
2017-04-03 22:20:24 +0800

    add static method

--- src/main/java/reflect/ReflectTest4.java
+++ src/main/java/reflect/ReflectTest4.java
@@ -14,11 +14,18 @@ public class ReflectTest4 {
         Class<?> clazz = Person.class;
         Method method = clazz.getMethod("print", String.class, int.class);
         method.invoke(new Person(), "liuruichao", 2);
+
+        method = clazz.getMethod("print");
+        method.invoke(new Person());
     }
 
     private static class Person {
         public void print(String name, int age) {
             System.out.println(String.format("name: %s, age: %s.", name, age));
+        }
+
+        public static void print() {
+            System.out.println("静态方法！");
         }
     }
 }
