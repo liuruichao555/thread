6b75153ea616ea0a0e0295875061f1e9ddff7d07
liuruichao <liuruichao555@126.com>
2017-04-03 22:17:59 +0800

    add method

---
+++ src/main/java/reflect/ReflectTest4.java
@@ -0,0 +1,24 @@
+package reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * ReflectTest4
+ *
+ * @author liuruichao
+ * Created on 2017/4/3 22:14
+ */
+public class ReflectTest4 {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> clazz = Person.class;
+        Method method = clazz.getMethod("print", String.class, int.class);
+        method.invoke(new Person(), "liuruichao", 2);
+    }
+
+    private static class Person {
+        public void print(String name, int age) {
+            System.out.println(String.format("name: %s, age: %s.", name, age));
+        }
+    }
+}
